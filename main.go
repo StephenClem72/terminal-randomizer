495b72940271e65c6187a5fff92a4fec791f4e5d
Stephen Clem <stephen.clem@inmar.com>
2020-05-29 11:01:37 -0500

    add main.go

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+  "os"
+	"os/exec"
+	"time"
+	"math/rand"
+	"github.com/common-nighthawk/go-figure"
+)
+
+func main() {
+	clear_terminal()
+	args := os.Args[1:]
+	rotations := 100
+	pluck_results(args, rotations)
+}
+
+func pluck_results(results []string, rotations int){
+	for i := 0; i < rotations; i++ {
+		index := rand.Int() % len(results)
+		value := strings.Title(results[index])
+		if i == (rotations - 1) {
+			ascii_print(value)
+			break
+		}
+		fmt.Println(value)
+		time.Sleep(100 * time.Millisecond)
+		clear_terminal()
+	}
+}
+
+func ascii_print(value string){
+	cap_value := strings.ToUpper(value)
+	fmt.Println(figure.NewFigure(cap_value, "dotmatrix", true))
+}
+
+func clear_terminal(){
+  cmd := exec.Command("clear")
+  cmd.Stdout = os.Stdout
+  cmd.Run()
+}
